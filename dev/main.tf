b8917ab6d1fba2a4012da383ecdc4c89a5b21f86
T R <thayshiva@gmail.com>
2024-09-22 10:22:23 +0100

    further updates

--- dev/main.tf
+++ dev/main.tf
@@ -18,3 +18,12 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
+terraform {
+  backend "s3" {
+    bucket     = "vel-tech-eu-west-1-terraform"
+    key        = "dev/terraform.tfstate"
+    region     = "eu-west-1"
+    encrypt    = true
+  }
+}
